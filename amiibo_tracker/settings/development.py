88d9aa364e7911669154ba5c14c4c7e0306a8bcc
Sina Serati <codingsina@gmail.com>
2025-07-06 14:37:34 -0500

    split the django env out

--- amiibo_tracker/settings/development.py
+++ amiibo_tracker/settings/development.py
@@ -15,13 +15,6 @@ DATABASES = {
     }
 }
 
-INSTALLED_APPS += [
-    "debug_toolbar",
-]
-
-MIDDLEWARE += [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-]
 
 INTERNAL_IPS = [
     "127.0.0.1",
