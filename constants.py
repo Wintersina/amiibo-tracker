afc140859dc28554666df080db0a4366b3cf11b6
Sina Serati <Wintersina@gmail.com>
2025-12-28 12:12:07 -0600

    Remove drive.file scope from OAuth

--- constants.py
+++ constants.py
@@ -7,7 +7,6 @@ class OauthConstants:
         "https://www.googleapis.com/auth/userinfo.email",
         "openid",
         "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive.file",
         "https://www.googleapis.com/auth/drive",
     ]
     REDIRECT_URI = os.getenv("OAUTH_REDIRECT_URI", "http://localhost:8000/oauth2callback/")
