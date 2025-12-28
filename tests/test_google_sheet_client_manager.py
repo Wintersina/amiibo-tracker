47799763d81c3e8e7386d57edf33d6321526895b
Sina Serati <Wintersina@gmail.com>
2025-12-28 00:18:00 -0600

    Add unit tests for sheet creation helpers

--- tests/test_google_sheet_client_manager.py
+++ tests/test_google_sheet_client_manager.py
@@ -2,6 +2,7 @@ import json
 
 import gspread
 import pytest
+import requests
 
 from tracker.google_sheet_client_manager import GoogleSheetClientManager
 
@@ -64,3 +65,104 @@ def test_get_or_create_worksheet_by_name_creates_defaults():
     assert amiibo_sheet.rows == [["Amiibo ID", "Amiibo Name", "Collected Status"]]
     assert config_sheet.rows == [["DarkMode"], ["0"]]
     assert manager.get_or_create_worksheet_by_name("AmiiboCollection") is amiibo_sheet
+
+
+def test_open_or_create_spreadsheet_reuses_existing():
+    existing_spreadsheet = object()
+
+    class DummyClient:
+        def open(self, name):
+            self.opened = name
+            return existing_spreadsheet
+
+        def create(self, name):  # pragma: no cover - should not be called
+            raise AssertionError("create should not be called when open succeeds")
+
+    manager = GoogleSheetClientManager(sheet_name="ExistingSheet")
+    manager.client = DummyClient()
+
+    result = manager._open_or_create_spreadsheet()
+
+    assert result is existing_spreadsheet
+    assert manager.client.opened == "ExistingSheet"
+
+
+def test_open_or_create_spreadsheet_creates_when_missing():
+    created_spreadsheet = object()
+
+    class DummyClient:
+        def __init__(self):
+            self.created = None
+
+        def open(self, name):
+            raise gspread.exceptions.SpreadsheetNotFound
+
+        def create(self, name):
+            self.created = name
+            return created_spreadsheet
+
+    manager = GoogleSheetClientManager(sheet_name="NewSheet")
+    manager.client = DummyClient()
+    manager.log_info = lambda *args, **kwargs: None
+
+    result = manager._open_or_create_spreadsheet()
+
+    assert result is created_spreadsheet
+    assert manager.client.created == "NewSheet"
+
+
+def test_open_or_create_spreadsheet_raises_on_create_failure():
+    class DummyClient:
+        def open(self, name):
+            raise gspread.exceptions.SpreadsheetNotFound
+
+        def create(self, name):
+            response = requests.Response()
+            response.status_code = 403
+            response._content = b"permission denied"
+            raise gspread.exceptions.APIError(response)
+
+    manager = GoogleSheetClientManager(sheet_name="FailingSheet")
+    manager.client = DummyClient()
+    manager.log_error = lambda *args, **kwargs: None
+    manager.log_info = lambda *args, **kwargs: None
+
+    with pytest.raises(ValueError) as excinfo:
+        manager._open_or_create_spreadsheet()
+
+    assert "could not be created" in str(excinfo.value)
+
+
+def test_initialize_default_worksheets_populates_missing():
+    manager = GoogleSheetClientManager()
+    spreadsheet = DummySpreadsheet()
+
+    manager._initialize_default_worksheets(spreadsheet)
+
+    amiibo_sheet = spreadsheet.worksheet(manager.work_sheet_amiibo_manager)
+    config_sheet = spreadsheet.worksheet(manager.work_sheet_config_manager)
+
+    assert amiibo_sheet.rows == [["Amiibo ID", "Amiibo Name", "Collected Status"]]
+    assert config_sheet.rows == [["DarkMode"], ["0"]]
+
+
+def test_initialize_default_worksheets_respects_existing_data():
+    manager = GoogleSheetClientManager()
+    spreadsheet = DummySpreadsheet()
+    spreadsheet.worksheets[manager.work_sheet_amiibo_manager] = DummyWorksheet(
+        manager.work_sheet_amiibo_manager
+    )
+    spreadsheet.worksheets[manager.work_sheet_amiibo_manager].rows.append([
+        "custom",
+    ])
+    spreadsheet.worksheets[manager.work_sheet_config_manager] = DummyWorksheet(
+        manager.work_sheet_config_manager
+    )
+    spreadsheet.worksheets[manager.work_sheet_config_manager].rows.append([
+        "config",
+    ])
+
+    manager._initialize_default_worksheets(spreadsheet)
+
+    assert spreadsheet.worksheet(manager.work_sheet_amiibo_manager).rows == [["custom"]]
+    assert spreadsheet.worksheet(manager.work_sheet_config_manager).rows == [["config"]]
