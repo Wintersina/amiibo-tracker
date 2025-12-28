d8432c2bbca478364dede61a4d2d7d687d5598f1
Sina Serati <Wintersina@gmail.com>
2025-12-28 12:10:45 -0600

    Add Drive scope to OAuth configuration

--- constants.py
+++ constants.py
@@ -8,5 +8,6 @@ class OauthConstants:
         "openid",
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive",
     ]
     REDIRECT_URI = os.getenv("OAUTH_REDIRECT_URI", "http://localhost:8000/oauth2callback/")
